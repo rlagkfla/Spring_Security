e1a710105f2c2561dcd97d8b26067a8b6a27a4cd
rlagkfla <khl9566@naver.com>
2023-05-14 21:09:39 +0900

    redirection error 230514

--- src/main/java/com/study/login2/config/WebSecurityConfig.java
+++ src/main/java/com/study/login2/config/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer configure(){ // static 하위 파일 목록(css, js, img) 인증 무시
 
-        return (web) -> web.ignoring().requestMatchers("/css/**", "/js/**", "/img/**");
+        return (web) -> web.ignoring().requestMatchers("/static/css/**", "/static/js/**", "/static/img/**");
 
     }
 
@@ -45,12 +45,12 @@ public class WebSecurityConfig {
                 .requestMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
                 .requestMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
-                .and()
+            .and()
                 .formLogin()
                     .loginPage("/login") // 로그인 페이지 링크
                     .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소 -> main.html
                     .permitAll()
-                .and()
+            .and()
                 .logout()
                     .logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
                     .invalidateHttpSession(true); // 세션 날리기
